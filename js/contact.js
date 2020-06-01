4e545f73e69f0fe45b7314e116935bc2b04da6d9
Jahaziel Martínez <jefedani45@comunidad.unam.mx>
2020-06-01 16:11:46 -0500

    Version2.0

---
+++ js/contact.js
@@ -0,0 +1,31 @@
+    const form = document.getElementById('contactForm'); // Obtenemos la referencia al formulario
+
+    if(form){ // Si existe nuestro elemento en memoria este se quedara escuchando al evento submit del formulario
+      form.addEventListener('submit', contactForm); // Al momento de enviar el formulario, ejecuta la función "contactform"
+    }
+
+    function contactForm(event) {
+      event.preventDefault(); // Prevenimos el comportamiento por defecto de un formulario (Enviar por URL los parametros)
+      const nombre = document.getElementById('nombre'); // Obtenemos la referencia a cada uno de nuestros elementos del formulario
+      const email = document.getElementById('email');
+      const mensaje = document.getElementById("mensaje");
+      const data = {
+        'name': nombre.value,
+        'email': email.value,
+        'mensaje': mensaje.value
+      }; // Creamos un objecto con todos los elementos de nuestro formulario.
+      saveContactForm(data); // Enviamos la información obtenida por el usuario a la función que se encargara de guardar la información en Firebase
+      form.reset(); // borramos todos los campos. 
+    }
+
+  function saveContactForm(data) {
+    firebase.database().ref('contact').push(data) // Hacemos referencia al método database de el SDK y hacemos referencia el nombre del objeto que contendrá nuestros registros y empujamos los nuevos envios de datos
+      .then(function(){
+        alert('Mensaje enviado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+      })
+      .catch(function(){
+        alert('mensaje No guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
+      });
+  };
+
+  
